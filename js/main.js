9aad3f36961ae12b54c6a5a5823f0144456bda46
Helen Hosain <helen.hosain@gmail.com>
2017-04-03 00:56:36 -0700

    V4 Updates
    Added jQuery to hero verticals and modal

--- js/main.js
+++ js/main.js
@@ -1,43 +1,110 @@
-// When the user mouseovers headline
-	// mouse enters the scene
-		// paragraph fades in and slides up
-	// mouse exits the scene
-		//paragraph fades out and slides down
+/* ----------------------------------------
+Event Dropdown
+------------------------------------------- */
 
+//when user clicks on event-container
+	//addClass active to topbar-content
+	//topbar background fades in 85%
 
-	
-// When the user clicks on one of the vertical hero images
-	// the vertical image expands to full cover display
-	
-
-	
-
-// $(function(){
-//   $('.event-list').find('.event').click(function(){
-//     $('.event-list').find('.openEv').removeClass('open');
-//     $(this).next().toggleClass('open');
-//   })
-// })
-
+//when user clicks close button
+	//removeClass active to topbar
+	//topbar background fades out
 
 $('.event-container').on('click', function () {
-    // Type code related to event here! :)
-    $('.topbar-background').fadeIn(300);
+ 	$('.topbar-background').fadeIn(300);  
     $('.topbar-content').addClass('active');
 });
 
 
 $('.close-button').on('click', function () {
-    // Type code related to event here! :)
     $('.topbar-background').fadeOut(300);
     $('.topbar-content').removeClass('active');
+
+});
+
+// GABY - How do i stop the page from reloading when clicking on "event-container"?
+
+
+/* ----------------------------------------
+Header Hover
+------------------------------------------- */
+
+// when the user hovers on "title"
+	//addClass "title-top"  to "title" to move up
+	//addClass "category-bottom" to "category"
+
+$('.header-media').mouseenter(function(){
+	$('.header-media .header-description').fadeTo(50, 1);
+	$('.header-media .header-description').addClass('active');
+	$('.header-media .header-title').addClass('title-up');
+	$('.header-media .header-category').addClass('category-down');
+});
+
+// GABY - How do I remove space between -title and category when mouse leaves?
+
+$('.header-media').mouseleave(function(){
+	$('.header-media .header-description').fadeTo(0, 0);
+	$('.header-media .header-description').removeClass('active');
+	$('.header-media .header-title').removeClass('title-up');
+	$('.header-media .header-category').removeClass('category-down');
 });
 
 
-//when user clicks on event-container
-//addClass active to topbar-content
-//topbar background fades in 85%
+$('.header-style').mouseenter(function(){
+	$('.header-style .header-description').fadeTo(50, 1);
+	$('.header-style .header-description').addClass('active');
+	$('.header-style .header-title').addClass('title-up');
+	$('.header-style .header-category').addClass('category-down');
+});
 
-//when user clicks close button
-//removeClass active to topbar
-//topbar background fades out
+// GABY - How do I remove space between -title and category when mouse leaves?
+
+$('.header-style').mouseleave(function(){
+	$('.header-style .header-description').fadeTo(0, 0);
+	$('.header-style .header-description').removeClass('active');
+	$('.header-style .header-title').removeClass('title-up');
+	$('.header-style .header-category').removeClass('category-down');
+});
+
+
+$('.header-events').mouseenter(function(){
+	$('.header-events .header-description').fadeTo(50, 1);
+	$('.header-events .header-description').addClass('active');
+	$('.header-events .header-title').addClass('title-up');
+	$('.header-events .header-category').addClass('category-down');
+});
+
+// GABY - How do I remove space between -title and category when mouse leaves?
+
+$('.header-events').mouseleave(function(){
+	$('.header-events .header-description').fadeTo(0, 0);
+	$('.header-events .header-description').removeClass('active');
+	$('.header-events .header-title').removeClass('title-up');
+	$('.header-events .header-category').removeClass('category-down');
+});
+
+$('.header-travel').mouseenter(function(){
+	$('.header-travel .header-description').fadeTo(50, 1);
+	$('.header-travel .header-description').addClass('active');
+	$('.header-travel .header-title').addClass('title-up');
+	$('.header-travel .header-category').addClass('category-down');
+});
+
+// GABY - How do I remove space between -title and category when mouse leaves?
+
+$('.header-travel').mouseleave(function(){
+	$('.header-travel .header-description').fadeTo(0, 0);
+	$('.header-travel .header-description').removeClass('active');
+	$('.header-travel .header-title').removeClass('title-up');
+	$('.header-travel .header-category').removeClass('category-down');
+});
+
+$('.media-image').on('click', function () {
+	$('.modal-background').css("display", "flex").hide().fadeIn(300);
+});
+
+$('.close-button').on('click', function () {
+	$('.modal-background').fadeOut(300);
+	
+});
+
